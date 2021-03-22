a6a61a37212976893b1e19727ecbcd107d36283b
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-22 23:18:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,8 @@ require 'pry'
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   reversed_sentence = []
-  reversed_sentence << sentence_array.each{|word| "word.reverse"}
-  reversed_sentence.join(" ")
+  reversed_sentence << sentence_array.each{|word| word.reverse}
+  #reversed_sentence.join(" ")
 end
 
 #binding.pry
